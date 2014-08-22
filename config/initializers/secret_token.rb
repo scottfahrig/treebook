3258e575a8265bf8a18ac90fa050e16d8cf716da
scottfahrig <sf@buildmidwestern.com>
2014-08-22 01:51:11 -0500

    created a rails application and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bandfund::Application.config.secret_token = '8a260c43da9021eef273d224f64be6b8c03b4eafefa2fac430cdc627f8292f813fe66e2a13602442c83461da869a410466b6807e7e9f0c4151aca6acd7de7061'
